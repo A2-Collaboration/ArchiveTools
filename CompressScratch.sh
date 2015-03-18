3a6cda8f3bd0ec104524afb3b5b56bb27dee7e9c
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-03-18 13:50:47 +0100

    Bump to 2015-03 beamtime

--- CompressScratch.sh
+++ CompressScratch.sh
@@ -4,9 +4,9 @@ set -e # fail on error
 cd /mnt/rundata/scratch
 # silently compress, use logfile to watch progress
 CompressAndCheck.sh --ignorenewest 1 --syslog 1 --delete 1 --worker 5 start >/dev/null 2>&1
-# after compression, move the 
+# after compression, move the
 # compressed files to the production folder
-BEAMTIME=2014-12_EPT_Prod
+BEAMTIME=2015-03_DoublePol_Deuteron
 mkdir -p ../$BEAMTIME
 cp MD5SUM ../$BEAMTIME
 mv -n *.dat.xz ../$BEAMTIME
