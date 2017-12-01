fef525cdf2cb91f57f9d09c0d83b1d63bd0485ea
Phil Martel <philmartel0@gmail.com>
2017-12-01 18:40:26 +0100

    Setup for current beamtime

--- CompressScratch.sh
+++ CompressScratch.sh
@@ -6,7 +6,7 @@ cd /mnt/rundata/scratch
 CompressAndCheck.sh --ignorenewest 1 --syslog 1 --delete 1 --worker 5 start >/dev/null 2>&1
 # after compression, move the
 # compressed files to the production folder
-BEAMTIME=2015-08_InMediumModifications
+BEAMTIME=2017-12_Compton
 mkdir -p ../$BEAMTIME
 cp MD5SUM ../$BEAMTIME
 mv -n *.dat.xz ../$BEAMTIME
