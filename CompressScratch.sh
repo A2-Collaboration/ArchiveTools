78f6bf6b0fdbe2d72a3769d30053e5e01bea9ca6
Phil Martel <philmartel0@gmail.com>
2017-12-21 14:30:13 +0100

    Fix for using storage instead of vmware1

--- CompressScratch.sh
+++ CompressScratch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH=/usr/local/bin:/usr/bin:/bin:/opt/ArchiveTools
 set -e # fail on error
-cd /mnt/rundata/scratch
+cd /mnt/storage1/scratch
 # silently compress, use logfile to watch progress
 CompressAndCheck.sh --ignorenewest 1 --syslog 1 --delete 1 --worker 5 start >/dev/null 2>&1
 # after compression, move the
